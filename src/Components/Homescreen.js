522de1c5ad375d3df0246d0ac0753cf64c095fbf
scottklein7 <scott.klein02@protonmail.com>
2021-12-30 14:28:23 -0500

    Initial commit

---
+++ src/Components/Homescreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../Homescreen.css'
+
+function Homescreen() {
+    return (
+        <div className="homeScreen">
+
+        </div>
+    )
+}
+
+export default Homescreen
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAPSV9DystL3V2umBHqzY_LMMaXhfl0H4U",
+    authDomain: "neflix-clone-5767e.firebaseapp.com",
+    projectId: "neflix-clone-5767e",
+    storageBucket: "neflix-clone-5767e.appspot.com",
+    messagingSenderId: "863708548591",
+    appId: "1:863708548591:web:35eb02ef7fe5572f3b3a1a"
+  };
